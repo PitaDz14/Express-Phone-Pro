44636e8b7c5862e601d2d369fe287ad50904c5d6
Khaled Deragha <aindeheb14tiaret@gmail.com>
2026-04-16 21:32:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7231922611-b9154",
+  "appId": "1:322540724646:web:9d20147d04fa2cf4fa6f67",
+  "apiKey": "AIzaSyCbcwojL7QXcSH9g7xmZAmD1KGKl_UWTPo",
+  "authDomain": "studio-7231922611-b9154.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "322540724646"
+};
